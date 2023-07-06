83bbbc101351a205f52d4ec8a8a3b855d8d0a953
ItsGamerBot47 <mauriojedagb47@gmail.com>
2023-07-06 17:50:25 -0500

    Sistema de correo + asunto + mensaje
    - Colocar asunto para enviar mensaje
    - Asignar destinatario. Codificado para identificar a correo en formato GMAIL
    - Mensaje con caracteres limitados a 300

--- Correo Ejemplo/CorreoEjemplo.js
+++ Correo Ejemplo/CorreoEjemplo.js
@@ -1,50 +1,72 @@
-const fromDb = undefined;
-
-var HTMLasunto = document.getElementById("txtAsunto");
-var HTMLcorreo = document.getElementById("txtCorreo");
-var HTMLmensaje = document.getElementById("txtArMensaje");
-var HTMLcontador = document.getElementById("ctMensaje");
-
-var asunto = '';
-var correo = '';
-var mensaje = '';
-var contador = 0;
+//  Obtener el elemento HTML
+let asunto = document.getElementById('txtAsunto');
+let correo = document.getElementById('txtCorreo');
+let mensaje = document.getElementById('txtArMensaje');
+let contador = document.getElementById('ctMensaje');
 
 function ValidarCorreo()
 {
     //  Validar texto en Asunto
-    if (asunto.length <= 0)
+    if (asunto.value.length <= 0)
     {
         alert("Ingresar asunto a enviar.");
+        return false;
     }
     //  Validar texto en Asunto
-    else if (correo.length <= 0)
+    else if (correo.value.length <= 0)
     {
         alert("Ingresar correo a enviar.");
+        return false;
     }
     //  Validar formato de email en Correo
-    else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(correo))
+    else if (!correo.value.indexOf('@gmail.com'))
     {
         alert("Ingresar un correo válido.")
+        return false;
     }
-    //  Validar texto en Mensaje
-    else if (mensaje.length <= 0)
+    //  Validar texto en Mensaje (0 a 300 caracteres)
+    else if (mensaje.value.length <= 0)
     {
         alert("Ingresar mensaje a enviar.");
+        return false;
     }
-    else if (300 < mensaje.length)
+    else if (300 < mensaje.value.length)
     {
         alert("Ingresar mensaje menor o igual a 300 caracteres.");
+        return false;
     }
+    return true;
 }
 
+//  Contar caracteres del mensaje
 function ContadorDeMensaje()
 {
     var contadorFinal = "";
-    contadorFinal += mensaje.length;
+    //  Contador de mensaje
+    contadorFinal += mensaje.value.length;
+    //  Límite
     contadorFinal += "/300";
-
-    contador = contadorFinal;
+    //  Resultado
+    contador.value = contadorFinal;
 }
 
-mensaje.onkeyup = ContadorDeMensaje();
+function EnviarGmail()
+{
+    if (ValidarCorreo())
+    {
+        var mailTo = "mailto:";
+        //  Añadir correo destinatario
+        mailTo += correo.value;
+        //  Añadir asunto
+        mailTo += "?subject=";
+        mailTo += asunto.value;
+        //  Añadir cuerpo
+        mailTo += "&body=";
+        mailTo += mensaje.value;
+        window.location.href = mailTo;
+    }
+}
+
+//  Actualizar contador al colocar mensaje
+mensaje.addEventListener('keypress', ContadorDeMensaje);
+mensaje.addEventListener('keyup', ContadorDeMensaje);
